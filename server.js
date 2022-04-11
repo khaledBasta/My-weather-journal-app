e74601082165535bbc764445be54fa49581a9239
khaled basta <khaled.basta@protonmail.com>
2022-04-11 08:04:08 +0200

    Writing get & post requests

--- server.js
+++ server.js
@@ -2,8 +2,13 @@
 let projectData = {};
 
 // Require Express to run server and routes
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { response } = require("express");
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,8 +16,26 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static("website"));
 
 // Setup Server
+const port = 3000;
+const server = app.listen(port, listening);
+function listening() {
+  // console.log(server);
+  console.log(`server is running on localhost: http://localhost:${port}`);
+}
+
+// get request
+app.get("/getData", (req, res) => {
+  res.send(projectData);
+});
+
+// post request
+app.post("/sendData", (req, res) => {
+  projectData = req.body;
+  res.send(projectData);
+});
--- website/app.js
+++ website/app.js
@@ -1,6 +1,38 @@
 /* Global Variables */
-let apiKey = "a40c6f048f9a89e079140d573655672d";
+const apiKey = "a40c6f048f9a89e079140d573655672d";
 
+let apiURL = "https://api.openweathermap.org/data/2.5/weather?zip=";
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+
+// get temp request
+const getTemp = async (apiURL, zipCode, apiKey) => {
+  const fetchTemp = await fetch(
+    apiURL + zipCode + "&appid=" + apiKey + "&units=metric"
+  );
+  try {
+    const data = await fetchTemp.json();
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+// post request
+const sendingData = async (data = {}) => {
+  const postReq = await fetch("/sendData", {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  try {
+    const result = await postReq.json();
+    return result;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
